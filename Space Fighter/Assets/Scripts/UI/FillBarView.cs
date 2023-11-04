a14a888f015a712e78e559b9626445bf4c1b7e3b
Joan Ginard Mateo <jgm7.hd@gmail.com>
2023-11-04 23:23:47 +0100

    lifebar animation

--- Space Fighter/Assets/Scripts/UI/FillBarView.cs
+++ Space Fighter/Assets/Scripts/UI/FillBarView.cs
@@ -5,14 +5,35 @@ namespace JGM.Game
 {
     public class FillBarView : MonoBehaviour
     {
-        [SerializeField] 
-        private Image m_fillImage;
+        [SerializeField] private Image m_whiteFillImage;
+        [SerializeField] private Image m_fillImage;
+        [SerializeField] private float m_lerpSpeed = 0.5f;
+
+        private float m_currentValue;
+        private float m_currentWhiteFillValue;
+        private float m_maxValue;
+        private float m_lerpTime;
 
         public void SetValue(float currentValue, float maxValue)
         {
-            float fillAmount = currentValue / maxValue;
-            Mathf.Clamp(fillAmount, 0, 1);
-            m_fillImage.fillAmount = fillAmount;
+            m_currentValue = currentValue;
+            m_maxValue = maxValue;
+            m_lerpTime = 0;
+        }
+
+        private void Update()
+        {
+            if (m_currentWhiteFillValue != m_currentValue)
+            {
+                m_currentWhiteFillValue = Mathf.Lerp(m_currentWhiteFillValue, m_currentValue, m_lerpTime);
+                m_lerpTime += m_lerpSpeed * Time.deltaTime;
+            }
+
+            float whiteFillAmount = m_currentWhiteFillValue / m_maxValue;
+            m_whiteFillImage.fillAmount = Mathf.Clamp(whiteFillAmount, 0, 1);
+
+            float fillAmount = m_currentValue / m_maxValue;
+            m_fillImage.fillAmount = Mathf.Clamp(fillAmount, 0, 1);
         }
     }
 }
