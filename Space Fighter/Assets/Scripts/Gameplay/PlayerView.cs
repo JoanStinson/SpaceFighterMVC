4d6d22956a3f0fb0d375138fda2ba879d6c3b970
Joan Ginard Mateo <jgm7.hd@gmail.com>
2023-11-04 18:31:46 +0100

    smooth player movement

--- Space Fighter/Assets/Scripts/Gameplay/PlayerView.cs
+++ Space Fighter/Assets/Scripts/Gameplay/PlayerView.cs
@@ -5,13 +5,17 @@ namespace JGM.Game
     public class PlayerView : MonoBehaviour
     {
         [SerializeField] private float m_moveSpeed = 0.1f;
-        [SerializeField] private float m_screenBoundaryPadding = 4f;
+        [SerializeField] private float m_smoothTime = 0.3f;
+        [SerializeField] private float m_moveScreenPadding = 4f;
+
+        private Vector3 m_velocity = Vector3.zero;
 
         private void Update()
         {
-            var newPosition = transform.position + Vector3.up * Input.GetAxis("Vertical") * m_moveSpeed * Time.deltaTime;
-            newPosition.y = Mathf.Clamp(newPosition.y, -m_screenBoundaryPadding, m_screenBoundaryPadding);
-            transform.position = newPosition;
+            Vector3 targetPosition = transform.position + Vector3.up * Input.GetAxis("Vertical") * m_moveSpeed * Time.deltaTime;
+            targetPosition.y = Mathf.Clamp(targetPosition.y, -m_moveScreenPadding, m_moveScreenPadding);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref m_velocity, m_smoothTime);
+            transform.position = targetPosition;
         }
     }
 }
