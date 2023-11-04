1f28fabc83f0053a98914958832c4794903b4b3a
Joan Ginard Mateo <jgm7.hd@gmail.com>
2023-11-04 18:23:20 +0100

    player movement

--- Space Fighter/Assets/Scripts/Gameplay/PlayerView.cs
+++ Space Fighter/Assets/Scripts/Gameplay/PlayerView.cs
@@ -1,10 +1,17 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace JGM.Game
 {
     public class PlayerView : MonoBehaviour
     {
-        
+        [SerializeField] private float m_moveSpeed = 0.1f;
+        [SerializeField] private float m_screenBoundaryPadding = 4f;
+
+        private void Update()
+        {
+            var newPosition = transform.position + Vector3.up * Input.GetAxis("Vertical") * m_moveSpeed * Time.deltaTime;
+            newPosition.y = Mathf.Clamp(newPosition.y, -m_screenBoundaryPadding, m_screenBoundaryPadding);
+            transform.position = newPosition;
+        }
     }
 }
