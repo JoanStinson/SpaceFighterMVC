e958cebdf11039a2e9a01ade6dc2ecb745df18a1
Joan Ginard Mateo <jgm7.hd@gmail.com>
2023-11-05 01:35:25 +0100

    parallax background

---
+++ Space Fighter/Assets/Scripts/Utils/ParallaxBackground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace JGM.Game
+{
+    public class ParallaxBackground : MonoBehaviour
+    {
+        [SerializeField] private float m_scrollSpeed = -100f;
+        [SerializeField] private float m_rightEdge = 41;
+        [SerializeField] private float m_leftEdge = -8;
+
+        private void Update()
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x + m_scrollSpeed * Time.deltaTime, transform.localPosition.y, transform.localPosition.z);
+
+            if (transform.localPosition.x <= m_leftEdge)
+            {
+                transform.localPosition = new Vector3(m_rightEdge, transform.localPosition.y, transform.localPosition.z);
+            }
+        }
+    }
+}
