3124fbba7ef6c4b4262ba086ab166c8ab472a854
Joan Ginard Mateo <jgm7.hd@gmail.com>
2023-11-05 19:15:14 +0100

    polished health bar

--- Space Fighter/Assets/Scripts/UI/FillBarView.cs
+++ Space Fighter/Assets/Scripts/UI/FillBarView.cs
@@ -5,35 +5,57 @@ namespace JGM.Game
 {
     public class FillBarView : MonoBehaviour
     {
+        [Header("White Fill")]
         [SerializeField] private Image m_whiteFillImage;
+        [SerializeField] private float m_whiteFillLerpSpeed = 0.03f;
+
+        [Header("Fill")]
         [SerializeField] private Image m_fillImage;
-        [SerializeField] private float m_lerpSpeed = 0.5f;
+        [SerializeField] private float m_fillLerpSpeed = 1f;
 
         private float m_currentValue;
         private float m_currentWhiteFillValue;
         private float m_maxValue;
-        private float m_lerpTime;
+
+        private float m_whiteFillLerpTime;
+        private float m_fillLerpTime;
 
         public void SetValue(float currentValue, float maxValue)
         {
             m_currentValue = currentValue;
             m_maxValue = maxValue;
-            m_lerpTime = 0;
+            m_whiteFillLerpTime = 0;
+            m_fillLerpTime = 0;
         }
 
         private void Update()
         {
+            RefreshWhiteFill();
+            RefreshFill();
+        }
+
+        private void RefreshWhiteFill()
+        {
             if (m_currentWhiteFillValue != m_currentValue)
             {
-                m_currentWhiteFillValue = Mathf.Lerp(m_currentWhiteFillValue, m_currentValue, m_lerpTime);
-                m_lerpTime += m_lerpSpeed * Time.deltaTime;
+                m_currentWhiteFillValue = Mathf.Lerp(m_currentWhiteFillValue, m_currentValue, m_whiteFillLerpTime);
+                m_whiteFillLerpTime += m_whiteFillLerpSpeed * Time.deltaTime;
             }
 
             float whiteFillAmount = m_currentWhiteFillValue / m_maxValue;
             m_whiteFillImage.fillAmount = Mathf.Clamp(whiteFillAmount, 0, 1);
+        }
 
+        private void RefreshFill()
+        {
             float fillAmount = m_currentValue / m_maxValue;
-            m_fillImage.fillAmount = Mathf.Clamp(fillAmount, 0, 1);
+            fillAmount = Mathf.Clamp(fillAmount, 0, 1);
+
+            if (m_fillImage.fillAmount != fillAmount)
+            {
+                m_fillImage.fillAmount = Mathf.Lerp(m_fillImage.fillAmount, fillAmount, m_fillLerpTime);
+                m_fillLerpTime += m_fillLerpSpeed * Time.deltaTime;
+            }
         }
     }
 }
