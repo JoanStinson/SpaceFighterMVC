4f093414a354dcefc83699f4838bd2e2f8b4d373
Joan Ginard Mateo <jgm7.hd@gmail.com>
2023-11-05 17:26:34 +0100

    enemy thruster animation

--- Space Fighter/Assets/Scripts/Gameplay/Enemies/EnemyShip/EnemyShipView.cs
+++ Space Fighter/Assets/Scripts/Gameplay/Enemies/EnemyShip/EnemyShipView.cs
@@ -4,9 +4,24 @@ namespace JGM.Game
 {
     public class EnemyShipView : EnemyView
     {
+        [Header("Enemy Ship Specifics")]
+        [SerializeField] private Transform m_thruster;
+
+        public override void Initialize(Vector3 startPosition, ComponentPool<EnemyView> pool, bool startMovingUp)
+        {
+            base.Initialize(startPosition, pool, startMovingUp);
+            m_thruster.gameObject.SetActive(true);
+        }
+
         protected override void Move()
         {
             transform.position -= Vector3.right * m_moveSpeed * Time.deltaTime;
+        }
+
+        public override void TakeDamage(float damageAmount)
+        {
+            base.TakeDamage(damageAmount);
+            m_thruster.gameObject.SetActive(false);
         }
     }
 }
